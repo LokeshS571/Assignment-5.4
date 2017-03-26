2b1e5c550ff6d4d2270590dc65bd0b2141e024a4
LokeshS571 <lokeshv542@gmail.com>
2017-03-26 14:11:55 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var Employee=new Array(5);
+Employee[0]= {name:"Lokesh S",age:21,salary:"3.3 lpa",address: {city:"Bengaluru", state:"Karnataka",pincode:560040}};
+Employee[1]= {name:"Sathya",age:20,salary:"3.3 lpa",address: {city:"Bengaluru", state:"Karnataka",pincode:560010}};
+Employee[2]= {name:"Vijay",age:20,salary:"3.3 lpa",address: {city:"Bengaluru", state:"Karnataka",pincode:560017}};
+Employee[3]= {name:"Balram",age:21,salary:"3.5 lpa",address: {city:"Bengaluru", state:"Karnataka",pincode:560019}};
+Employee[4]= {name:"Umesh",age:20,salary:"3.4 lpa",address: {city:"Bengaluru", state:"Karnataka",pincode:560020}};
+
+
+
+
+
+
+
+// to display in console
+console.log("Employee Name :",Employee[4].name); 
+console.log("Age :",Employee[4].age);
+console.log("Salary :",Employee[4].salary);
+console.log("City :",Employee[4].address.city);
+console.log("State :",Employee[4].address.state);
+console.log("Pincode :",Employee[4].address.pincode);
